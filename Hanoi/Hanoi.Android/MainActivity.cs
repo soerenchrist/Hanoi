a9636923e2b31cdf85d361db3a2cc5c99a617a71
Sören Christ <s.christ@mailbox.org>
2021-11-05 23:06:38 +0100

    Add colors

--- Hanoi/Hanoi.Android/MainActivity.cs
+++ Hanoi/Hanoi.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.OS;
 
 namespace Hanoi.Droid
 {
-    [Activity(Label = "Hanoi", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ScreenOrientation = ScreenOrientation.Landscape, MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Hanoi", Theme = "@style/MainTheme", ScreenOrientation = ScreenOrientation.Landscape, MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)

58d5793784e7bd2bed95847c9c78a00833f20561
Sören Christ <soeren.christ@azo.com>
2021-11-08 14:18:29 +0100

    Add platform initialization for ios

--- Hanoi/Hanoi.iOS/AppDelegate.cs
+++ Hanoi/Hanoi.iOS/AppDelegate.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using Prism;
+using Prism.Ioc;
 using UIKit;
 
 namespace Hanoi.iOS
@@ -11,7 +13,7 @@ namespace Hanoi.iOS
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IPlatformInitializer
     {
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
@@ -23,9 +25,13 @@ namespace Hanoi.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
+            LoadApplication(new App(this));
 
             return base.FinishedLaunching(app, options);
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
         }
     }
 }
